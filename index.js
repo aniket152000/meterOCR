2fd2f7712db8f88229de6b38a8ffae549b329dc8
aniket152000 <aniketghare24@gmail.com>
2023-05-23 15:45:44 +0530

    logic changed

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const multer = require('multer');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 const Tesseract = require('tesseract.js');
 
 const app = express();
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
-const upload = multer({ dest: 'uploads/' });
-
-app.post('/read', upload.single('image'), async (req, res) => {
+app.post('/ocr', async (req, res) => {
   try {
-    const imagePath = req.file.path;
-    const result = await Tesseract.recognize(imagePath, 'eng', {
-      tessedit_char_whitelist: '0123456789', // Specify the characters to recognize (numbers only)
-    });
-    //const meterReading = result.data.text.replace(/\s/g, ''); // Remove whitespace from the recognized text
-    const meterReading = result.data.text.replace(/[^0-9]/g, '').substring(1,6);
-    //meterReading=result.data.text.replace(/\s/g, '');
+    const { imageUrl } = req.body;
 
-    res.json({ reading: meterReading });
+    const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    const imageBuffer = Buffer.from(imageResponse.data, 'binary');
+    const result = await Tesseract.recognize(imageBuffer, 'eng');
+    const ocrText = result.data.text.replace(/[^0-9]/g, '').substring(0, 6);
+
+    res.status(200).json({ ocrText });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'An error occurred' });
   }
 });
 
-
 app.listen(port, "0.0.0.0", function () {
   console.log(`Server is running on port ${port}`);
 });
+
+
